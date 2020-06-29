f7f2b3610ff7b3328854b6f2c66bc9f762475335
thatprakhar <pnahar@purdue.edu>
2020-06-29 14:16:12 +0530

    Change interval speed

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var num;
 var rects = [];
-var bubbleFlag = true;
+
 var beingSorted = false;
-var sortedRect = [];
-var speed = 100;
+var speed = 25;
 
 class rectangles {
   constructor(xPos, height, ctx, color) {
@@ -81,6 +80,10 @@ function bubbleSort() {
         beingSorted = false;
         console.log("exited out of bubble sort");
         clearInterval(interval);
+        for (var k = 0; k < num; k++) {
+          delete rects[k];
+          console.log("delete");
+        }
         return;
       }
       if (rects[j].height > rects[j + 1].height) {
@@ -157,6 +160,10 @@ function selectionSort() {
     if (i >= num) {
       console.log("exited out of selection sort");
       beingSorted = false;
+      for (var k = 0; k < num; k++) {
+        delete rects[k];
+        console.log("delete");
+      }
       clearInterval(interval);
       i = 0;
     }
@@ -192,8 +199,6 @@ function insertionSort() {
   var min = 2000;
   console.log("insertion sort running");
   var interval = setInterval(function() {
-    var min_index = i;
-
     var j = i;
 
     while (j - 1 >= 0 && rects[j].height < rects[j - 1].height) {
@@ -216,10 +221,139 @@ function insertionSort() {
     i++;
     if (i >= num) {
       console.log("exited out of insertion sort");
+      for (var k = 0; k < num; k++) {
+        delete rects[k];
+        console.log("delete");
+      }
       beingSorted = false;
       clearInterval(interval);
       i = 0;
     }
   }, speed);
   console.log("exited finally out of insertion");
+}
+
+function mergeSort() {
+  if (beingSorted == true) return;
+  num = document.getElementById("num").value;
+  if (num == "") {
+    console.log("NUM is NULL\n");
+    beingSorted = false;
+    return;
+  }
+
+  beingSorted = true;
+
+  console.log("num is " + num);
+  var pos = 0;
+  ctx.fillStyle = "#333333";
+  ctx.fillRect(0, 0, 1000, 500);
+  for (var i = 0; i < num; i++) {
+    rects[i] = new rectangles(
+      pos,
+      Math.floor(Math.random() * 500),
+      ctx,
+      getRandomColor()
+    );
+    rects[i].draw();
+    pos += 1000 / num;
+    rects[i].draw();
+  }
+  console.log("Here");
+  mergesort(0, num - 1);
+  beingSorted = false;
+  console.log("exited finally out of merge");
+  for (var x = 0; x < num; x++) console.log(rects[x].height);
+}
+
+async function mergesort(low, high) {
+  if (low < high) {
+    var mid = (low + high) / 2;
+    mid = Math.floor(mid);
+    mergesort(low, mid);
+    mergesort(mid + 1, high);
+    merge(low, mid, high);
+  }
+}
+
+async function merge(l, m, r) {
+  var n1 = m - l + 1;
+  var n2 = r - m;
+
+  var L = [];
+  var R = [];
+  var k = l;
+  var i = 0,
+    j = 0;
+
+  for (var p = 0; p < n1; p++) {
+    L[p] = new rectangles(
+      rects[l + p].pos,
+      rects[l + p].height,
+      rects[l + p].ctx,
+      rects[l + p].color
+    );
+  }
+  for (var p = 0; p < n2; p++) {
+    R[p] = new rectangles(
+      rects[m + 1 + p].pos,
+      rects[m + 1 + p].height,
+      rects[m + 1 + p].ctx,
+      rects[m + 1 + p].color
+    );
+  }
+  while (i < n1 && j < n2) {
+    if (i < n1 && j < n2) {
+      if (L[i].height < R[j].height) {
+        rects[k].color = L[i].color;
+        rects[k].height = L[i].height;
+        rects[k].yPos = 500 - rects[k].height;
+        i++;
+      } else {
+        rects[k].color = R[j].color;
+        rects[k].height = R[j].height;
+        rects[k].yPos = 500 - rects[k].height;
+        j++;
+      }
+      k++;
+      ctx.fillStyle = "#333333";
+      ctx.fillRect(0, 0, 1000, 500);
+      for (var p = 0; p < num; p++) {
+        rects[p].draw();
+        await new Promise(r => setTimeout(r, 2000));
+      }
+    }
+  }
+  interval = 0;
+  while (i < n1) {
+    rects[k].color = L[i].color;
+    rects[k].height = L[i].height;
+    rects[k].yPos = 500 - rects[k].height;
+    i++;
+    k++;
+    ctx.fillStyle = "#333333";
+    ctx.fillRect(0, 0, 1000, 500);
+    for (var p = 0; p < num; p++) {
+      rects[p].draw();
+      await new Promise(r => setTimeout(r, 2000));
+    }
+  }
+
+  while (j < n2) {
+    rects[k].color = R[j].color;
+    rects[k].height = R[j].height;
+    rects[k].yPos = 500 - rects[k].height;
+    j++;
+    k++;
+    ctx.fillStyle = "#333333";
+    ctx.fillRect(0, 0, 1000, 500);
+    for (var p = 0; p < num; p++) {
+      rects[p].draw();
+      await new Promise(r => setTimeout(r, 2000));
+    }
+  }
+  for (var i = 0; i < n1; i++) {
+    delete L[i];
+  }
+  for (var i = 0; i < n2; i++) delete R[i];
 }
